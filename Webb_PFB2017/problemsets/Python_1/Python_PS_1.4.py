12c300529d4292767bf713a9b5ace40712fd13b4
admin <admin@pfb17.cshl.edu>
2017-10-17 11:33:03 -0400

    Add Python PS 1.4

---
+++ Webb_PFB2017/problemsets/Python_1/Python_PS_1.4.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import sys
+
+name = sys.argv[1]
+fav_color = sys.argv[2]
+fav_activity = sys.argv[3]
+fav_animal = sys.argv[4]
+
+print("My name is " + name + ".")
+print("My favorite colour " +  fav_color + ".")
+print("My favorite activity is " + fav_activity + ".")
+print("My favorite animal is " + fav_animal + ".")
+
+ 
