0d5089c058b6f4552e5166a426e3d95ffacbc508
admin <admin@pfb17.cshl.edu>
2017-10-17 16:35:29 -0400

    Adding the correct version, prev version was in python2, this is in python3

--- Webb_PFB2017/problemsets/Python_2/Python_PS_2.8.py
+++ Webb_PFB2017/problemsets/Python_2/Python_PS_2.8.py
@@ -5,24 +5,18 @@ import sys
 my_number = float(sys.argv[1])
 
 if my_number > 0 :			#This line tests if my_number is positive.
-	message = "is positive."
-	print my_number, message	
+	print (my_number, 'is positive.')	
 	if my_number > 50 :
-		message = 'is greater than 50.'
-		print my_number, message
+		print (my_number, 'is greater than 50.')
 		if my_number%3 == 0:
-			message = 'is divisble by 3.'
-			print my_number, message
+			print (my_number, 'is divisible by 3.')
 	elif my_number < 50 :
-		message = 'is less than 50.'
-		print my_number, message
+		print (my_number, 'is less than 50.')
 		if my_number%2 == 0:
-			message = 'is even.'
-			print my_number, message
-elif my_number < 0 :			#This line tests if my_number is negative.
-	message = "is negative."
-	print my_number, message
+			print (my_number, 'is even.')
+elif my_number < 0 :			#This line tests if my_number is negative."
+	print (my_number, 'is negative.')
 
-print "Script is complete!" 
+print ("Script is complete!") 
 
 
