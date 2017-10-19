9d1ddd701ca38ab133413074ff9a837d1f661ab5
admin <admin@pfb17.cshl.edu>
2017-10-18 20:15:19 -0400

    adding PS 4.3

--- Webb_PFB2017/problemsets/Python_4/Python_PS_4.3.py
+++ Webb_PFB2017/problemsets/Python_4/Python_PS_4.3.py
@@ -2,7 +2,18 @@
 
 numbers = [101,2,15,22,95,33,2,27,72,15,52]
 
-for num in numbers:
-	if num %2 == 0:
-		print(num,"is even.")
-	
+sorted_numbers = sorted(numbers)	
+even_sum = 0
+odd_sum = 0
+
+for number in sorted_numbers:
+	print(number)
+	if number %2 == 0:
+		even_sum = even_sum + number
+		number += 1
+	elif number %2 != 0:
+		odd_sum = odd_sum + number
+		number += 1
+
+print("The sum of all even numbers is:", even_sum)
+print("The sum of all odd numbers is:", odd_sum)
