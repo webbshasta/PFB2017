bbc0ae48ba360f5ea95d208238f95bd8d98df6ef
admin <admin@pfb17.cshl.edu>
2017-10-18 20:25:31 -0400

    adding PS 4.5

---
+++ Webb_PFB2017/problemsets/Python_4/Python_PS_4.5.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+#Write a script that uses range() in a for loop to print out every number between 0 and 99
+
+for num in range(0, 100):
+	print(num)
+for num in range(1, 101):
+	print(num)
+
