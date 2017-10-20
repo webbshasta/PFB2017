529e3a58cc173a0c64241a5c2d6ad3353841ba31
admin <admin@pfb17.cshl.edu>
2017-10-20 10:40:50 -0400

    updating repo

---
+++ Webb_PFB2017/problemsets/Python_6/Python_PS_6.7.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import re
+
+#The enzyme ApoI has a restriction site: R^AATTY where R and Y are degenerate nucleotideides. See the IUPAC table to identify the nucleotide possibilities for the R and Y. Write a regular expression to find and print all occurrences of the site in the following sequence Python_06_ApoI.fasta.
+
+#Y can be C or T
+#R can be A or G
+
+fasta_open = open('Python_06_ApoI.fasta', 'r')
+
+for line in fasta_open:
+	line = line.rstrip()
+	if not line.startswith('>'):
+		for 
+
+ in re.findall(r'([A|G])AATT([C|T)]', line):
+			print(ApoI)
+
+print("Script has finished.")
