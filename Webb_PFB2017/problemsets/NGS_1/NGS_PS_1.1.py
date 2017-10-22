af34d167d97227d59a4472d1e23b29a96644a483
admin <admin@pfb17.cshl.edu>
2017-10-22 17:03:31 -0400

    updating 1.1 to output mean and standard dev

--- Webb_PFB2017/problemsets/NGS_1/NGS_PS_1.1.py
+++ Webb_PFB2017/problemsets/NGS_1/NGS_PS_1.1.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-import subprocess
+import re
+import statistics
 
 #Write a script to count the number of sequences in a FASTQ file.
 #Output the mean and standard deviation of sequence lengths
@@ -8,10 +9,32 @@ import subprocess
 
 fastq_input = open('pfb.fastq', 'r')
 
-count = 0
+line_counter = 1
+seq_count = 0
+lengths = [] #creating empty list of sequence lengths
+quality_scores = [] #creating empty list of q scores
 
 for line in fastq_input:
 	line = line.strip()
-	count += 1
+	if line_counter%4 == 1:
+		seq_count += 1
+	elif line_counter%4 == 2:
+		lengths.append(len(line))#find length and add to length list
+	elif line_counter%4 == 0:
+		for score in line:
+			quality_scores.append(ord(score) + 33)#translate phred score & add to quality score list
+	line_counter += 1
 
-print(int(count/4))
+print('Number of sequences:', seq_count)
+print('Mean sequence length:', sum(lengths)/len(lengths))
+print('Standard dev. of sequence length: {:.2f}'.format((statistics.stdev(lengths))))
+print('Mean quality score: {:.2f}'.format(sum(quality_scores)/len(quality_scores)))
+print('Standard dev. of quality score: {:.2f}'.format(statistics.stdev(quality_scores)))
+ 
+
+
+
+
+
+
+
